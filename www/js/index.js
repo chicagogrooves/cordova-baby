db719ecc78840ffe0876a5ed73459bdebac1e9ce
Dean Radcliffe <dradcliffe@opinionlab.com>
2014-10-06 13:29:04 -0500

    Successful querying of EmployeeService

--- www/js/index.js
+++ www/js/index.js
@@ -44,7 +44,7 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        console.log('DeviceReady: Received Event: ' + id);
     }
 };
 
